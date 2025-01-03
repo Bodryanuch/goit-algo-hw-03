98780729ee1dc4c7cf038b72420598e98190e580
Bodryanuch <odarchukbogdan@gmail.com>
2025-01-03 19:56:15 +0200

    Code cutting

--- Task1.py
+++ Task1.py
@@ -1,43 +1,11 @@
-from datetime import datetime#, timedelta
-import re
+from datetime import datetime
 
+today = datetime.now()     # Сьогоднішня дата
+given_date = "2020-10-09"
 
-today = datetime.now().date()
-given_date = input("enter the date in the format year-month-day (YYYY-MM-DD):")
-format_date = r"\d{4}-\d{2}-\d{2}"    # шаблон відповідності введеного формату дати
+def get_days_from_today(date):
+    checked_date = datetime.strptime(date, "%Y-%m-%d")       # Переведення рядка в об'єкт дататайм
+    difference_day = today.toordinal() - checked_date.toordinal()      # Дізнаємося різницю в днях
+    return difference_day     # Повертаємо результат
 
-
-def checked_date(date):      # Функція обробки вийнятків
-    checked_given_date = None
-    clone_date = date
-
-    while checked_given_date == None:    # цикл який запитує введення даних доки данні не будуть коректними
-        match = re.search(format_date, clone_date)
-
-        if match == None:      # Якщо не відповідає формату введення данних то
-            print(f"{"_" * 30}\nInvalid input format\n{"_" * 30}")
-            clone_date = input("enter the date in the format year-month-day (YYYY-MM-DD):")
-
-        else:
-            try:    # Якщо формат введення дати правельний то переводимо рядок в об'єкт datetime
-                checked_given_date = datetime.strptime(match.group(), "%Y-%m-%d")
-            except:    # На випадок якщо задана дата не існує
-                print(f"{"_" * 30}\nDate does not exist\n{"_" * 30}")
-                clone_date = input("enter the date in the format year-month-day:")
-
-    return checked_given_date.date()
-
-
-def get_days_from_today(date):     # функція різниці в днях між датами
-    difference_day = checked_date(date).toordinal() - today.toordinal()
-
-    if difference_day < 0:    # Якщо дата вже пройшла то вивести:
-        print(f"{abs(difference_day)} days have passed since the given date")
-
-    elif difference_day > 0:     # Якщо дата в майбутньому то вивести:
-        print(f"There are {difference_day} days left until the given date")
-
-    else:    # якщо дата сьогодні то вивести:
-        print("today's date is entered")
-
-get_days_from_today(given_date)
+print(get_days_from_today(given_date))
--- Task2.py
+++ Task2.py
@@ -1,63 +1,9 @@
 import random
 
 
-min_value = None
-max_value = None
-quantity = None
-
-
-while type(min_value) != int:   # Перевірка на правильність введення початку лотереї
-    try:    # якщо введуть ціле число то
-        min_value = int(input(f"{"_"*30}\nEnter the minimum lottery value"))
-
-        if min_value <= 0:     # якщо початок лотереї менше 0 включно прохання ввести початок заново
-            print(f"{"_"*30}\nMinimum lottery value cannot be less than 0")
-            min_value = None
-        # Тут я б ще додав обмеження на верхній діапазон але в задачі такого вказано не було,
-        # можете прийняти за помилку
-
-        #elif min_value >= 998:      # Але вирішив що якщо потрібна всетаки то нехай полежить тут)
-            #print(f"{"_" * 30}\nMinimum lottery value cannot be more than 998")
-            #min_value = None
-
-    except:    # Якщо ввели не число або не ціле число, прохання ввести коректні данні
-        print(f"{"_"*30}\nInvalid value, try again")
-        min_value = None
-
-
-while type(max_value) != int:       # Перевірка на правильність введення максимального порогу лотереї
-    try:    # якщо введуть ціле число
-        max_value = int(input(f"{"_"*30}\nEnter the maximum lottery value"))
-
-        if max_value <= min_value:    # Прохання ввести заново якщо введений верхній порог менше нижнього
-            print(f"{"_" * 30}\nMaximum lottery value cannot be less than minimum value")
-            max_value = None
-
-        elif max_value > 1000:      # Прохання ввести заново якщо верхній порог бідьше 1000
-            print(f"{"_" * 30}\nMaximum lottery value cannot be more then 1000")
-            max_value = None
-
-    except:      # Якщо ввели не число або не ціле число, прохання ввести коректні данні
-        print(f"{"_" * 30}\nInvalid value, try again")
-        max_value = None
-
-
-while type(quantity) != int:     # Перевірка на правильність введеня кількості виграшних позицій
-    try:
-        quantity = int(input(f"{"_"*30}\nEnter the number of winning positions"))
-
-        if quantity <= 0:       # Якщо кількість виграшних позицій менше або дорівнює 0 прохання переввести
-            print(f"{"_" * 30}\nThere must be at least one winning number in the lottery")
-            quantity = None
-
-        elif quantity >= max_value - min_value:      # Якщо кількість позицій перевищує діапазон виграшних
-                                                     # номерів прохання переввести данні
-            print(f"{"_" * 30}\nThere cannot be more winning positions than positions in total")
-            quantity = None
-
-    except:     # Якщо ввели не число або не ціле число, прохання ввести коректні данні
-        print(f"{"_" * 30}\nInvalid value, try again")
-        quantity = None
+min_value = 1
+max_value = 49
+quantity = 6
 
 
 def get_number_ticket(min_value, max_value, quantity):  # функція на генерацію виграшної комбінації

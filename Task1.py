ccca24ba12c931ed48ae20d3f8fc5582c008681d
Bodryanuch <odarchukbogdan@gmail.com>
2025-01-04 22:48:45 +0200

    Code refinement

--- Task1.py
+++ Task1.py
@@ -1,11 +1,14 @@
 from datetime import datetime
 
-today = datetime.now()     # Сьогоднішня дата
+
 given_date = "2020-10-09"
 
+
 def get_days_from_today(date):
+    today = datetime.now()  # Сьогоднішня дата
     checked_date = datetime.strptime(date, "%Y-%m-%d")       # Переведення рядка в об'єкт дататайм
     difference_day = today.toordinal() - checked_date.toordinal()      # Дізнаємося різницю в днях
     return difference_day     # Повертаємо результат
+
 
 print(get_days_from_today(given_date))
--- Task2.py
+++ Task2.py
@@ -1,19 +1,30 @@
 import random
 
 
-min_value = 1
-max_value = 49
-quantity = 6
+def get_numbers_ticket(min, max, quantity):  # функція на генерацію виграшної комбінації
+    win_number = []      # створюємо порожній список\
 
+    # Якщо мінімальне число не відповідає параметрам задачі повернути порожній список
+    # (обмеження по максимуму в задачі не вказано)
+    if min <= 0 or min > 999:
+        pass
 
-def get_number_ticket(min_value, max_value, quantity):  # функція на генерацію виграшної комбінації
-    win_number = []      # створюємо порожній список
-    while len(win_number) != quantity:     # цикл працює поки список не набере потрібну кількість комбінацій
-        number = random.randrange(min_value, max_value + 1)    # Генерація випадкового числа в заданих межах
+    # Якщо максимальне число не відповідає параметрам задачі повернути порожній список
+    elif max <= min or max > 1000:
+        pass
 
-        if win_number.count(number) == 0:   # Якщо згенерованого числа ще не має в списку то додати його
-            win_number.append(number)
+    # Якщо кількість елементів унікального списку задана невірно повернути поржній список
+    elif quantity >= max - min or quantity <= 0:
+        pass
+
+    else:
+        while len(win_number) != quantity:     # цикл працює поки список не набере потрібну кількість комбінацій
+            number = random.randrange(min, max + 1)    # Генерація випадкового числа в заданих межах
+
+            if win_number.count(number) == 0:   # Якщо згенерованого числа ще не має в списку то додати його
+                win_number.append(number)
+
     return win_number     # Повернути список з функції
 
 
-print(get_number_ticket(min_value, max_value, quantity))
+print(get_numbers_ticket(0, 100, 6))
